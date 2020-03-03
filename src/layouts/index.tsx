b59813a653e092b5fd72fd78950d3a4110c57b92
2075 <marco@one.io>
2020-03-04 00:53:48 +0100

    feat(pro): fix floating layout fs err

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,4 +1,4 @@
 import Fixed from './Fixed'
 export { default as Fixed } from './Fixed'
-export { default as Floating } from './Floating'
+export { default as Liquid } from './Liquid'
 export default Fixed
