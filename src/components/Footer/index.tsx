065afd45ec8da7650dbe040d72e1ff2a9c0359cf
2075 <marco@one.io>
2020-03-09 22:15:33 +0100

    feat(home): split content, add decrypt

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -48,8 +48,8 @@ const Footer: React.FC = ({}) => {
 			<Flex flexDirection="row" flexWrap="wrap">
 				<Box px={4} width={[1, 1, 1 / 2, 3 / 5]}>
 					<Text>
-						zero.io is building gameDAO, a decentralised autonomous organisation, summoned to foster sustainability and improve financing in
-						videogames, creative content, digital arts, e-sports and gaming as a profession.
+						zero.io is building gameDAO, a decentralised autonomous organisation, summoned to foster sustainability and improve financing in video
+						games, creative content, digital arts, e-sports and gaming as a profession.
 						<br />
 						<br />
 						gameDAO provides matchmaking and crowd based funding instruments, connecting and empowering loyal gamers, creators, publishers and
--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 
 import { Flex, Box, Heading, Text } from 'rebass'
@@ -9,14 +9,13 @@ import logo from 'public/z-ctrl-45-wht.svg'
 
 import { IoIosArrowRoundDown } from 'react-icons/io'
 
-const Items = () => (
-	<Box width={[1]} p={[4]}>
-		<Text textAlign="left" fontSize={[5, 6, 7]} fontWeight={800} pb={2}>
-			GameDAO is a cooperative creating crowdfinance tools for video game creators, publishers, investors and gamers.
-		</Text>
+import { useDecrypt } from 'src/lib/useDecrypt'
+
+const Items = title => (
+	<>
 		<Box width={[1, 1, 3 / 4]} pt={4}>
 			<Text fontSize={[4, 5]} pb={12}>
-				Finance is the hard part in creating games, creative content or becoming a professional gamer.
+				Funding is one of the hardest parts in creating games, creative content or becoming a professional gamer.
 			</Text>
 		</Box>
 		<Box width={[1, 1, 3 / 4]}>
@@ -35,8 +34,8 @@ const Items = () => (
 			</Text>
 		</Box>
 		<Box width={[1, 1 / 2]} pt={4}>
-			<Text textAlign="left" fontSize={[2]} fontWeight={600}>
-				Get an invite before we launch:
+			<Text textAlign="left" fontSize={[2]} fontWeight={600} color="#ff00cc">
+				Get an invite before we launch.
 			</Text>
 			<Newsletter />
 		</Box>
@@ -47,19 +46,51 @@ const Items = () => (
 				</Link>
 			</Heading>
 		</Box>
-	</Box>
+	</>
 )
 
-const Home = () => (
-	<Container>
-		<Intro>
-			<Flex flexDirection="row" flexWrap="wrap">
+const Home = () => {
+	const { result, decrypt } = useDecrypt()
+	const [title, setTitle] = useState('')
+
+	const t = 'GameDAO creates crowdfinance tools for video game creators, publishers, investors and gamers.'
+
+	useEffect(() => {
+		handleChangeTitle(t)
+	}, [])
+
+	const handleChangeTitle = title => {
+		decrypt(t)
+		setTitle(t)
+	}
+
+	return (
+		<Container>
+			<Intro>
+				<Flex flexDirection="row" flexWrap="wrap">
+					<Box width={[1]} px={[4, 6]} pt={[2, 4]}>
+						<Box width={[1]} p={[4]}>
+							<Text textAlign="left" fontSize={[5, 6, 7]} fontWeight={800} pb={2}>
+								{result}
+							</Text>
+						</Box>
+						<Box width={1}>
+							<Heading textAlign="center" fontSize={[3]} pt={5}>
+								<Link href="/#content">
+									<IoIosArrowRoundDown size="48" />
+								</Link>
+							</Heading>
+						</Box>
+					</Box>
+				</Flex>
+			</Intro>
+			<Flex id="content" flexDirection="row" flexWrap="wrap">
 				<Box width={[1]} px={[4, 6]} pt={[2, 4]}>
 					<Items />
 				</Box>
 			</Flex>
-		</Intro>
-	</Container>
-)
+		</Container>
+	)
+}
 
 export default Home
