e4a8e50f65bf9a09a0d8abb9e22727ee28c1d2b1
2075 <marco@one.io>
2020-03-09 11:28:59 +0100

    feat(react-spring): react-spring beta

--- config/env.ts
+++ config/env.ts
@@ -6,7 +6,7 @@ export const FB_TRACKING_ID = process.env.FB_TRACKING_ID
 export const SITE_NAME = process.env.SITE_NAME || 'gameDAO'
 export const SITE_TITLE = process.env.SITE_TITLE || 'Crowdfunding for Videogames'
 export const SITE_DESCRIPTION =
-	process.env.SITE_DESCRIPTION || 'a DAO for videogames , creative content and gaming as a profession'
+	process.env.SITE_DESCRIPTION || 'a DAO for videogames, creative content and gaming as a profession'
 export const SITE_IMAGE = process.env.SITE_IMAGE || 'https://gamedao.co/z-control-45-full.png'
 export const SITE_TW_NAME = process.env.SITE_TW_NAME || '@zerodotio'
 
