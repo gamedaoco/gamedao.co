66652894112681140761139b895fce9b7b1caf71
2075 <marco@one.io>
2020-04-10 00:00:36 +0200

    feat(projects): add projects page

---
+++ src/containers/Blank.tsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+
+import { Flex, Box, Heading, Text } from 'rebass'
+import { Link, Intro, Container, ContentGrid } from 'components'
+
+import img from 'public/img/1.png'
+import logo from 'public/z-ctrl-45-wht.svg'
+
+import { IoIosArrowRoundDown } from 'react-icons/io'
+
+const Page = () => {
+	return (
+		<Container>
+			<Flex id="content" flexDirection="row" flexWrap="wrap" color="rgba(255,255,255,.65)">
+				<Box width={[1]} px={[4, 6]} py={[2, 4]}>
+					<ContentGrid />
+				</Box>
+			</Flex>
+		</Container>
+	)
+}
+
+export default Page
---
+++ src/pages/projects.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+import Layout from 'layouts'
+import { Projects } from 'containers'
+
+const Page = () => (
+	<Layout>
+		<Projects />
+	</Layout>
+)
+
+export default Page
