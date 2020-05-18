792d4899e8b220e53ef9b09b7c7409ec895bbb75
2075 <marco@one.io>
2020-05-18 15:12:58 +0200

    feat(config): fix typo

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
 export const DEV = process.env.NODE_ENV !== 'production'
-export const HOST = DEV ? 'http://localhost:3030/api/' : 'https://gamedao.com/api/'
+export const HOST = DEV ? 'http://localhost:3030/api/' : 'https://gamedao.co/api/'
 export const ENV = DEV ? 'dev' : 'production'
 export const KEY = 'hello'
