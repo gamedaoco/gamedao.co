47df1df0e8d53980a74815cae490c77d4d2185b8
2075 <marco@one.io>
2020-05-22 01:01:49 +0200

    feat(link): auto blank on external link

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -2,16 +2,21 @@ import * as React from 'react'
 import NextLink from 'next/link'
 
 interface ILinkProps {
-	href?: string | undefined
-	as?: string | undefined
-	target?: string | undefined
+	href?: string
+	as?: string
+	target?: string
 	children?: object | string
 }
 
-const Link: React.FC<ILinkProps> = ({ href, as, target, children }) => (
-	<NextLink href={href || '#'} as={as}>
-		<a target={target || '_self'}>{children}</a>
-	</NextLink>
-)
+const Link: React.FC<ILinkProps> = ({ href, as, target, children }) =>
+	href && href.indexOf('http') > -1 ? (
+		<a href={href} target="_blank">
+			{children}
+		</a>
+	) : (
+		<NextLink href={href || '#'} as={as}>
+			<a target={target || '_self'}>{children}</a>
+		</NextLink>
+	)
 
 export default Link
