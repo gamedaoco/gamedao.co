48ede9fd7bc23a57281d8d9eb8ea1f4ee7deff0b
2075 <marco@one.io>
2020-05-21 10:30:21 +0200

    feat(pages): content updates

--- src/pages/campaigns.tsx
+++ src/pages/campaigns.tsx
@@ -1,12 +1,25 @@
-import * as React from 'react'
+/**
+           _______________________________ ________
+           \____    /\_   _____/\______   \\_____  \
+             /     /  |    __)_  |       _/ /   |   \
+            /     /_  |        \ |    |   \/    |    \
+           /_______ \/_______  / |____|_  /\_______  /
+                   \/        \/         \/         \/
+		   Z  E  R  O  .  I  O     N  E  T  W  O  R  K
+		   © C O P Y R I O T   2 0 7 5   Z E R O . I O
+**/
+
+import { useContext } from 'react'
+import { AppContext } from 'src/hooks/AppContext'
 
 import Layout from 'layouts'
 import { Campaigns } from 'containers'
 
-const Page = () => (
-	<Layout>
-		<Campaigns />
-	</Layout>
-)
+const Page = () => {
+	const { state } = useContext(AppContext)
+	const { SHOW_CAMPAIGNS } = state.features.data
+
+	return <Layout>{SHOW_CAMPAIGNS && <Campaigns />}</Layout>
+}
 
 export default Page
