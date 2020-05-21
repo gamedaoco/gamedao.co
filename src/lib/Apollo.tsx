79b00f890f822cd2758c3d722c02b04b8b4d1f84
2075 <marco@one.io>
2020-05-22 00:59:11 +0200

    feat(context): update hooks

--- src/lib/Apollo.tsx
+++ src/lib/Apollo.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { AppContext } from 'src/hooks/AppContext'
 import fetch from 'isomorphic-unfetch'
 
@@ -13,57 +13,63 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 
 const initialState = {}
 
-let uri
-let GQL_DEVKEY
-
-const cache = new InMemoryCache().restore( initialState || {} )
-
-const apiLink = new HttpLink({
-	uri,
-	fetch,
-	credentials: 'same-origin', // include, *same-origin, omit
-})
-
-const logoutLink = onError(( { networkError }: ErrorResponse ) => {
-	if (
-		networkError &&
-		'statusCode' in networkError &&
-		networkError.statusCode === 401
-	) {
-		//logout();
-		console.error('authorization error')
-	}
-})
-
-const authLink = setContext((_, { headers }) => {
-	const token = localStorage.getItem('token')
-	console.warn('access token', token)
-
-	return {
-		headers: {
-			...headers,
-			'x-hasura-admin-secret': GQL_DEVKEY,
-			// Authorization: token ? `Bearer ${token}` : '',
-		},
-	}
-})
-
-const client = new ApolloClient({
-	ssrMode: true,
-	link: from([ authLink, logoutLink, apiLink ]),
-	cache: cache,
-})
-
 export const Apollo = ({ children }) => {
 
 	const { state } = useContext( AppContext )
-	const { GQL_URI } = state.config.data
+	const { GQL_URI, GQL_KEY } = state.config.data
+
+
+	// useEffect(()=>{
+
+	// 	console.log('changed')
+
+	// },[ GQL_URI, GQL_KEY ])
+
+		const cache = new InMemoryCache().restore( initialState || {} )
+
+		const apiLink = new HttpLink({
+			uri: GQL_URI,
+			fetch,
+			credentials: 'same-origin', // include, *same-origin, omit
+		})
+
+		const logoutLink = onError(( { networkError }: ErrorResponse ) => {
+			if (
+				networkError &&
+				'statusCode' in networkError &&
+				networkError.statusCode === 401
+			) {
+				//logout();
+				console.error('authorization error')
+			}
+		})
+
+		const authLink = setContext((_, { headers }) => {
+			const token = localStorage.getItem('token')
+
+			// console.warn('key', GQL_KEY)
+
+			return {
+				headers: {
+					...headers,
+					'x-hasura-admin-secret': GQL_KEY,
+					// Authorization: token ? `Bearer ${token}` : '',
+				},
+			}
+		})
+
+		let client = new ApolloClient({
+			ssrMode: true,
+			link: from([ authLink, logoutLink, apiLink ]),
+			cache: cache,
+		})
 
 	return (
-		<ApolloProvider client={client}>
-			{children}
+		<ApolloProvider client={ client }>
+		{ children }
 		</ApolloProvider>
 	)
+
 }
 
 export default Apollo
