4e4aa159d38f7158679e64d4e11d536a38b61446
2075 <marco@one.io>
2020-05-22 00:56:27 +0200

    feat(graphql): collect queries

---
+++ src/queries/projects.ts
@@ -0,0 +1,29 @@
+import gql from 'graphql-tag'
+
+export const GET_PROJECTS_TOP = gql`
+	query getTopProjects {
+		projects(order_by: { project_raise_amount: desc }, limit: 2) {
+			project_name
+			project_pitch
+			project_id
+			project_image
+			project_raise_start
+			project_pitch_key
+			funding_wallet
+		}
+	}
+`
+
+export const GET_PROJECTS_ALL = gql`
+	query getCurrentProjects($limit: Int!, $offset: Int!) {
+		projects(order_by: { project_raise_start: desc }, limit: $limit, offset: $offset) {
+			project_name
+			project_pitch
+			project_id
+			project_image
+			project_raise_start
+			project_pitch_key
+			funding_wallet
+		}
+	}
+`
