6a5ec56641534521ff2c3faf3c71295e7bdb73e6
2075 <marco@one.io>
2020-05-23 17:54:50 +0200

    feat(host): assemble uri

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,9 @@
 export const DEV = process.env.NODE_ENV !== 'production'
 export const ENV = DEV ? 'dev' : 'production'
-export const HOST = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/api/` : 'http://localhost:3030/api/'
 export const KEY = 'hello'
+export const HOST =
+	typeof window !== 'undefined'
+		? window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port : '') + '/api/'
+		: '/api/'
+
+// process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/api/` : 'http://localhost:3030/api/'
