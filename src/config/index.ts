43cd14abede9a062195f3d11a920a7db920ea7b8
2075 <marco@one.io>
2020-05-23 19:45:55 +0200

    feat(ci): config based on branch

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,7 @@
-export const DEV = process.env.NODE_ENV !== 'production'
+export const DEV = process.env.VERCEL_GITLAB_COMMIT_REF !== 'master'
 export const ENV = DEV ? 'dev' : 'production'
 export const KEY = 'hello'
 export const HOST =
 	typeof window !== 'undefined'
 		? window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port : '') + '/api/'
 		: '/api/'
-
-// process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}/api/` : 'http://localhost:3030/api/'
