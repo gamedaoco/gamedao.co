816670c3c542c7512f00152be50c931ff6a75741
2075 <marco@one.io>
2020-11-23 16:33:36 +0100

    feat(layout): update layout

--- src/layouts/Liquid.tsx
+++ src/layouts/Liquid.tsx
@@ -1,3 +1,14 @@
+/**
+           _______________________________ ________
+           \____    /\_   _____/\______   \\_____  \
+             /     /  |    __)_  |       _/ /   |   \
+            /     /_  |        \ |    |   \/    |    \
+           /_______ \/_______  / |____|_  /\_______  /
+                   \/        \/         \/         \/
+           Z  E  R  O  .  I  O     N  E  T  W  O  R  K
+           © C O P Y R I O T   2 0 7 5   Z E R O . I O
+**/
+
 import React, { useContext } from 'react'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
