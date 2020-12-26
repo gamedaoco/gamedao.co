a78ff33d649ef217aee70ec5469906cd5b9b1546
2075 <marco@one.io>
2020-12-26 23:48:41 +0100

    feat(header): update header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -19,10 +19,8 @@ const log = Logger('$HEADER')
 
 // context
 
-import { AppContext } from 'src/context/AppContext'
+import { useAppContext } from 'src/context/AppContext'
 import { useSubstrate } from 'src/context/SubstrateContext'
-import { useQuery } from '@apollo/react-hooks'
-import { GET_FX } from 'src/queries'
 
 // ui
 
@@ -34,85 +32,58 @@ import { RatesWrapper, Container, MetaLink, User, Task, Right } from './styles'
 import logo from 'public/z-ctrl-45-col.svg'
 import img from 'public/stripes-s.png'
 
-// import AccountSelector from 'src/components/substrate/AccountSelector'
+//
 
-// const Rates = () => {
-// 	const { loading, error, data } = useQuery(GET_FX)
+import AccountSelector from 'src/components/substrate/AccountSelector'
 
-// 	if (loading) return <RatesWrapper>Loading...</RatesWrapper>
-// 	if (error) return null // <RatesWrapper>Error: ${error.message}</RatesWrapper>
+const loader = (text) => (
+	<Dimmer active>
+		<Loader size="small">{text}</Loader>
+	</Dimmer>
+)
 
-// 	const fx = data.fx
-// 	console.log(data)
+const message = (err) => (
+	<div>
+		<Message negative compact floating header="Error connecting to Network" content={`${JSON.stringify(err)}`} />
+	</div>
+)
 
-// 	return (
-// 		<RatesWrapper>
-// 			{fx.map((rate) => (
-// 				<Link key={rate.id} href="/account/wallet">
-// 					{rate.name}&nbsp;{rate.askQuote || 0}
-// 				</Link>
-// 			))}
-// {/*
-// 			{<Link href="/account/wallet#xp">XP&nbsp;{ token.XP || 0 }</Link>}
-// 			{<Link href="/account/wallet#rep">REP&nbsp;{ token.REP || 0 }</Link>}
-// 			{<Link href="/account/wallet#play">PLAY&nbsp;{ token.PLAY || 0 }</Link>}
-// */}
-// 		</RatesWrapper>
-// 	)
-// }
+const ConnectBtn = styled.div`
+	padding: 10px;
+	background: #ff0000;
+	color: #ffffff;
+`
 
-// const loader = (text) => (
-// 	<Dimmer active>
-// 		<Loader size="small">{text}</Loader>
-// 	</Dimmer>
-// )
+const Header = () => {
+	// app state
 
-// const message = (err) => (
-// 	<div>
-// 		<Message negative compact floating header="Error connecting to Network" content={`${JSON.stringify(err)}`} />
-// 	</div>
-// )
+	const { state } = useAppContext()
+	const { user } = state
+	const { SHOW_CONTACT, SHOW_CAMPAIGNS, SHOW_APPLY, SHOW_FX, SHOW_SIGNIN } = state.features.data
 
-type HeaderProps = {
-	// email?: string
-	message?: string
-	loggedIn?: boolean
-	showContactView?: boolean
-}
+	// connectivity & user
 
-const Header: React.FC<HeaderProps> = ({ loggedIn }) => {
-	const { state } = useContext(AppContext)
-	const { SHOW_CAMPAIGNS, SHOW_APPLY, SHOW_FX, SHOW_SIGNIN } = state.features.data
-	const { CONNECTED } = state.web3
+	const { CONNECTED } = state.net
+	const hasUser = user.state === 'SIGNEDIN' ? true : false
+	if (hasUser) log.info(user)
+	else log.info('no user')
 
-	// const [showAccountSelector, setAccountSelector] = useState(false)
-	// const [accountAddress, setAccountAddress] = useState(null)
+	// const [ showAccountSelector, toggleAccountSelector ] = useState( false )
+	// const [ accountAddress, setAccountAddress ] = useState( null )
 
-	// const { apiState, keyring, keyringState, apiError } = useSubstrate()
+	// const { api, apiState, keyring, keyringState, apiError } = useSubstrate()
+
 	// const accountPair = accountAddress && keyringState === 'READY' && keyring.getPair(accountAddress)
 
 	// useEffect(() => {
-	// 		if (apiState === 'ERROR') return message('Error')
-	// 		else if (apiState !== 'READY') return loader('Connecting to Network')
-	// 		if (keyringState !== 'READY') {
-	// 			return loader("Loading accounts (please review any extension's authorization)")
-	// 		}
-	// }, [])
 
-	// const token = {
-	// 	XP: 1337,
-	// 	REP: 49152,
-	// 	PLAY: 13.37,
-	// 	ZERO: 1000,
-	// 	GAME: 1000,
-	// 	// exchange rates
-	// 	// stable rate should be 1 EUR == 1 PLAY
-	// 	EUR_PLAY: 1,
-	// 	// current exchange rate for ETH to PLAY via Exchange
-	// 	DAI_PLAY: 0.92,
-	// 	// current exchange rate for ETH to EUR via Exchange
-	// 	ETH_EUR: 321.02,
-	// }
+	// 	if (apiState !== 'READY' || !api) return
+	// 	if (apiState === 'ERROR') return message('Error')
+	// 	else if (apiState !== 'READY') return loader('Connecting to Network')
+	// 	if (keyringState !== 'READY')
+	// 	return loader("Loading accounts (please review any extension's authorization)")
+
+	// }, [])
 
 	const router = useRouter()
 	// const showContactView = router.pathname == '/contact' ? false : true
@@ -122,7 +93,7 @@ const Header: React.FC<HeaderProps> = ({ loggedIn }) => {
 			{/*<Divider sm />*/}
 
 			<Flex flexDirection="row" flexWrap="wrap">
-				{/* TOP */}
+				{/* LOGO */}
 
 				<Box order={[1]} pt={4} px={4} py={3} width={[1, 1 / 4]}>
 					<Link href="/">
@@ -130,22 +101,19 @@ const Header: React.FC<HeaderProps> = ({ loggedIn }) => {
 					</Link>
 				</Box>
 
-				<Box order={[3, 2]} pt={[0, 4]} px={4} width={[1, 1 / 4]}>
-					<MetaLink>{/* {SHOW_FX && <Rates />} */}</MetaLink>
-				</Box>
-
-				<Box order={[2, 3]} pt={4} px={4} width={[1, 1 / 4]}>
+				<Box order={[2, 3]} pt={4} px={4} width={[1, 3 / 4]}>
 					<MetaLink>
-						{loggedIn && <Link href="/account">Account</Link>}
+						{hasUser && <Link href="/account">Account</Link>}
 
 						{SHOW_APPLY && <Link href="/apply">Apply</Link>}
 						{SHOW_CAMPAIGNS && <Link href="/campaigns">Campaigns</Link>}
 
-						{loggedIn && <Link href="/doc/help">Helpdesk</Link>}
-						{!loggedIn && <Link href="/contact">Contact</Link>}
+						{hasUser && <Link href="/doc/help">Helpdesk</Link>}
+						{!hasUser && SHOW_CONTACT && <Link href="/contact">Contact</Link>}
 
-						{!loggedIn && <Link href="/account/signup">Sign Up</Link>}
-						{loggedIn && <Link href="/account/signout">Sign&nbsp;Out</Link>}
+						{!hasUser && <ConnectBtn>Sign In</ConnectBtn>}
+						{!hasUser && <Link href="/account/signup">Sign Up</Link>}
+						{hasUser && <Link href="/account/signout">Sign&nbsp;Out</Link>}
 
 						{/*<Link onClick={setAccountSelector(true)}>Sign In</Link>*/}
 						{/*{showAccountSelector && <AccountSelector setAccountAddress={setAccountAddress} />}*/}
@@ -170,15 +138,15 @@ const Header: React.FC<HeaderProps> = ({ loggedIn }) => {
 						</Task>
 					)}
 				</Box>
-
-*/}
+				*/}
 
 				{/* NAVBAR */}
+
 				{/*
 				<Box order={5} width={[1]}>
 					<Navigation />
 				</Box>
-*/}
+				*/}
 			</Flex>
 		</Container>
 	)
