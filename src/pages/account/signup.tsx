b839dbd174f74d29466bb26d98bed3d20ef61701
2075 <2075@zero.io>
2021-01-31 11:18:49 +0100

    feat(account): init signup

--- src/pages/account/signup.tsx
+++ src/pages/account/signup.tsx
@@ -9,15 +9,26 @@
 			 © C O P Y R I O T   2 0 7 5   Z E R O . I O
 **/
 
-import * as React from 'react'
+import { useState, useEffect } from 'react'
+import styled from 'styled-components'
 
-import Layout from 'layouts'
-import { Home } from 'containers'
+import { Flex, Box, Heading, Text } from 'rebass'
+import { Link, Intro, Container } from 'components'
 
-const Page = () => (
-	<Layout>
-		<h1>Sign Up</h1>
-	</Layout>
-)
+import img from 'public/img/1.png'
+import logo from 'public/z-ctrl-45-wht.svg'
+
+import { IoIosArrowRoundDown } from 'react-icons/io'
+
+const Page = () => {
+	return (
+		<Container>
+			<Flex id="content" flexDirection="row" flexWrap="wrap" color="rgba(255,255,255,.65)">
+				<Box width={[1]} px={[4, 6]} py={[2, 4]}>
+				</Box>
+			</Flex>
+		</Container>
+	)
+}
 
 export default Page
