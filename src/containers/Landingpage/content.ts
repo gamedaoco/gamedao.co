9d75e1c188747cab70dd5499a0372bb75198eac2
2075 <2075@zero.io>
2022-04-22 00:02:22 +0200

    feat(landingpage): add content, style fragments

---
+++ src/containers/Landingpage/content.ts
@@ -0,0 +1,167 @@
+export const team = [
+
+	[ 'Marco', 		'Founder', 			'/img/marco.png', 	'https://linkedin.com/in/marcobahn' ],
+	[ 'Yonghan', 	'Partnerships', 	'/img/yonghan.png', 'https://linkedin.com/in/yonghanlee' ],
+	[ 'Inan', 		'Product', 			'/img/inan.png', 	'https://linkedin.com/in/inangueder' ],
+	[ 'Tjorven', 	'User Experience', 	'/img/tjorven.png', 'https://linkedin.com/in/tjorvenfitz' ],
+
+	[ 'Dirk', 		'Tokenization', 	'/img/marco.png', 	'https://linkedin.com/in/dirkkruwinnus' ],
+	[ 'Olof', 		'Legal', 			'/img/yonghan.png', 'https://linkedin.com/in/olofsundberg' ],
+	[ 'Joanna', 	'Communications',	'/img/inan.png', 	'https://linkedin.com/in/joannachan' ],
+	[ 'Mike', 		'User Experience',	'/img/tjorven.png', 'https://linkedin.com/in/mikeplanzer' ],
+
+	[ 'JKLNZ', 		'Growth Hacker', 	'/img/jklnz.png', 	'https://linkedin.com/in/dirkkruwinnus' ],
+	[ 'Kitty', 		'Growth Hacker',	'/img/kitty.png', 	'https://linkedin.com/in/olofsundberg' ],
+	[ 'Pascal', 	'Technical Writer',	'/img/huegli.png', 	'https://linkedin.com/in/pascalhuegli' ],
+	[ 'Anon',	 	'Writer',			'/img/anon.png', 	'https://linkedin.com/in/anonymouse' ],
+
+	[ 'Ava', 		'Author',			'/img/ava.png', 	'https://linkedin.com/in/avalocked' ],
+	[ 'BERB', 		'Visual Artist',	'/img/berb.png', 	'https://linkedin.com/in/lukasberbier' ],
+
+]
+
+export const partners = [
+
+	[ 'Acala',	'Network', '/img/acala.png', 'https://acala.network' ],
+	[ 'Kilt', 	'Identity', '/img/kilt.png', 'https://kilt.io' ],
+	[ 'Fractal','Identity', '/img/fractal.png',	'https://fractal.id' ],
+
+]
+
+export const supporters = [
+
+	[ 'Scytale Ventures',	'',	'/img/scytale.png', 'https://' ],
+	[ 'Valhalla Capital',	'',	'/img/valhalla.png', 'https://' ],
+	[ 'Automata Network', 	'',	'/img/automata.png', 'https://' ],
+	[ 'MaxStealth',			'',	'/img/maxstealth.png', 'https://' ],
+
+	[ 'YaoQi',				'',	'/img/yaoqi.png', 'https://' ],
+	[ '1PAR Research',		'',	'/img/1par.png', 'https://' ],
+	[ 'NXGEN',				'',	'/img/maxstealth.png', 'https://' ],
+	[ 'Tek Forest',			'',	'/img/tekforest.png', 'https://' ],
+
+	[ 'Outlier Ventures',	'',	'/img/maxstealth.png', 'https://' ],
+
+]
+
+//
+//
+//
+
+export const section1 = [
+	{
+		title: 'Create Better Games',
+		description: 'Probably the most random thing you have ever seen!',
+		image: '/s01/1__128c.png',
+		link_text: 'Moaer',
+		link_url: '#'
+	},
+	// {
+	// 	title: `Build Decentralized Video Game Economies`,
+	// 	description: 'Just another stinky text',
+	// 	image: ``,
+	// 	link_url: '#'
+	// }
+]
+
+
+export const section2 = {
+	header1: `Join GameDAO`,
+	text1: `With more than 2.7 billion gamers worldwide,
+		community driven ownership and game creation should be
+		the standard for the video game industry.
+		From forging the initial idea over collaboration
+		to fundraising and finally creating game economies,
+		it is vital to provide gamers with a safe environment to collaborate.`,
+	header2: `A THRIVING COMMUNITY OF GAMERS & CREATORS`,
+	text2: `And with GameDao we are doing just that:
+		Providing an open protocol dedicated to bridge
+		the world of video games with the world of DAO, DeFi and NFTs
+		to shift the empowerment back into the hands of the community.`
+}
+
+export const section3 = {
+	text: `LET’S BRING COMMUNITY POWERED OWNERSHIP TO THE MASSES`
+}
+
+export const section4 = {
+	title: `How we are transforming the Gaming Industry`,
+	content: [
+		`WE ENABLE CONTRIBUTING
+		Creators can fund their projects
+		Gamers can support teams they love
+		Offering investors a new industry
+		traditional fundraising + borrowing of funds`,
+
+		`WE ENABLE COORDINATION
+		democratic governance + ownership for teams
+		Integrating gamers as active stakeholders
+		Creating more sustainable business models
+		Enable early market proof for projects`,
+
+		`WE PROVIDE TRUST
+		Providing Ecosystem-wide decentralised identities
+		common treasury powering community initiatives
+		A utility $GAME token for access and governance
+		Transparent Due Diligence and Compliance tools`
+	]
+}
+
+export const section5 = {
+	title: `We are a Community Centered Organization`,
+// left
+// 	image
+// 		arvr gamer
+// 	copy
+// 		“Finally I can support the games I love without being just an exploitable resource.”
+// 	list
+// 		usp
+// 		usp
+// 		usp
+
+// right
+// 	image
+// 		coder hacker
+// 	copy
+// 		“It should be the experience, that is touching.
+// 		What we strive for is to make the person playing the game the director of it.”
+// 	list
+// 		usp
+// 		usp
+// 		usp
+}
+
+export const section6 = {
+	title: `THE CONNECTOME FOR THE GAMER & CREATOR ECOSYSTEM`,
+	text: `
+		Your invitation to GameDAO is waiting:
+		We have opened up the private beta and
+		invite you to catch one of the rare access keys now
+		to be among the first to play and talk with Hawkins
+		and maybe even get your hands on an ultra rare edition of it.`,
+	url: `https://beta.gamedao.co` 
+}
+
+export const section7 = {
+	title: `Team`
+}
+
+export const section8 = [{
+		title: `launch partners`,
+		items: [
+			`kilt`,
+			`fractal.id`,
+			`acala`,
+	]},{
+		title: `investors`,
+		items: [
+			`scytale`,
+			`automata`,
+			`1par`,
+			`tekforest`,
+			`outlier ventures`,
+			`vyoma`,
+			`nxgen`,
+			`yaoqi`,
+		]
+	}]
---
+++ src/containers/Landingpage/styles.ts
@@ -0,0 +1,4 @@
+export const GRADIENT = {
+	pink: '-webkit-linear-gradient(45deg, #ff00cc 30%, #ff9900 90%)',
+	blue: '-webkit-linear-gradient(45deg, #00ffff 30%, #00ff66 90%)'
+}
