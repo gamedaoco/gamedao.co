30e08ad1352583f5b5512e53060a3a5770530f91
2075 <2075@zero.io>
2022-06-06 21:04:53 +0200

    feat: rm shadow

--- src/containers/Landingpage/modules/Transform.tsx
+++ src/containers/Landingpage/modules/Transform.tsx
@@ -6,7 +6,7 @@ import { List, ListItem, ListItemIcon } from '@mui/material'
 import { Hero, Team, Partners, Supporters } from './'
 
 export const Transform = () => (
-	<Paper sx={{ px:'2rem', backgroundColor: 'none', height: '100%' }} elevation={0}>
+	<Paper sx={{ px:'2rem', backgroundColor: 'transparent', height: '100%' }} elevation={0}>
 		<Grid container spacing={[0,2,4]} pt={[0,2]}>
 			<Grid key={'title'} container item xs={12} direction="row" >
 				<Typography variant={'h3'} py={2}>{transform.title} </Typography>
@@ -14,7 +14,7 @@ export const Transform = () => (
 			{ transform.content.map( (item, i) => {
 				return(
 					<Grid key={i} container item xs={12} sm={4} direction="column" >
-						<Card sx={{ p:0, m:0, background: 'none', height: '100%' }} >
+						<Card sx={{ boxShadow: 0, p:0, m:0, background: 'none', height: '100%' }} >
 							<Typography variant={'h4'} pb={2}>{item.title} </Typography>
 							<List>
 							{ item.content.map( ( listItem, j) => {
